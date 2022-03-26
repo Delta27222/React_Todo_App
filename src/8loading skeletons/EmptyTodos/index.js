4e41168ef8e66d54f266ed87105ae4f066650322
Angel <angelgabriel.hernandez@gmail.com>
2022-03-26 16:38:13 -0600

    d

--- src/8loading skeletons/EmptyTodos/index.js
+++ src/8loading skeletons/EmptyTodos/index.js
@@ -5,7 +5,6 @@ function EmptyTodos(){
     return(
         <div className="container_error1">
             <h3 className="tittle1">No tasks were found!!</h3>
-            {/* <h3 className="tittle1">Task not found!!</h3> */}
             <img className="img1" src="https://img.icons8.com/emoji/96/000000/pensive-face.png"/>
         </div>
     )
