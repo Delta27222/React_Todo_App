a28786fd488524ec477a7d5edc3c0285eada9f79
Angel <angelgabriel.hernandez@gmail.com>
2022-03-26 18:33:42 -0600

    Using the SkeletonLoading mediaQuery

--- src/8loading skeletons/TodosLoading/index.js
+++ src/8loading skeletons/TodosLoading/index.js
@@ -72,35 +72,6 @@ function TodosLoading(){
                 </div>
             </div>
 
-            <div className="skeleton-container">
-                <div className="s1-skeleton">
-                    <div className="icon-skeleton animate-skeleton"></div>
-                </div>
-                <div className="s2-skeleton">
-                    <div className="title-skeleton animate-skeleton"></div>
-                    <div className="text-skeleton animate-skeleton"></div>
-                    <div className="text-skeleton animate-skeleton"></div>
-                    <div className="text-skeleton animate-skeleton"></div>
-                </div>
-                <div className="s3-skeleton">
-                    <div className="delete-skeleton animate-skeleton"></div>
-                </div>
-            </div>
-
-            <div className="skeleton-container">
-                <div className="s1-skeleton">
-                    <div className="icon-skeleton animate-skeleton"></div>
-                </div>
-                <div className="s2-skeleton">
-                    <div className="title-skeleton animate-skeleton"></div>
-                    <div className="text-skeleton animate-skeleton"></div>
-                    <div className="text-skeleton animate-skeleton"></div>
-                    <div className="text-skeleton animate-skeleton"></div>
-                </div>
-                <div className="s3-skeleton">
-                    <div className="delete-skeleton animate-skeleton"></div>
-                </div>
-            </div>
         </li>
     )
     
