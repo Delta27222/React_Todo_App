fd34f3d8c0fe5db87ccdaf350e9301401928e36c
Angel <angelgabriel.hernandez@gmail.com>
2022-04-09 10:59:49 -0500

    Problem in DeleteTodo resolve

--- src/4todoItem/index.js
+++ src/4todoItem/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { TodoContext } from "../TodoContext";
 import './TodoItem.css';
 
 // Imagenes svg
@@ -8,33 +7,32 @@ import { ReactComponent as DeleteIcon }  from "../assets/delete.svg";
 const path1 = 'https://img.icons8.com/external-febrian-hidayat-outline-color-febrian-hidayat/64/000000/external-check-ui-essential-febrian-hidayat-outline-color-febrian-hidayat.png'
 const path2 = 'https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/48/000000/external-setting-essentials-pack-tanah-basah-glyph-tanah-basah.png'
 
-function TodoItem(props) {
-
-  const {numberOfTask} = React.useContext(TodoContext)
+function TodoItem({key, text, completed, id, setId, onComplete, onDelete, setOpenModal, setAction, setTodoText, numberOfTask}) {
 
   const showModalEdit = (event) => {
-    props.setOpenModal(prevState => !prevState);
-    props.setTodoText(props.text);
-    props.setAction("editTodo");
-    props.setId(props.id);
+    setOpenModal(prevState => !prevState);
+    setTodoText(text);
+    setAction("editTodo");
+    setId(id);
   }
   const showModalConfirmDelete = (event) => {
-    props.setOpenModal(prevState => !prevState);
-    props.setAction("deleteTodo");
-    props.setTodoText(props.text);
+    setOpenModal(prevState => !prevState);
+    setAction("deleteTodo");
+    setTodoText(text);
+    setId(id);
   }
 
   return (
     <li className="TodoItem grid_container">
         <img
-          src={`${props.completed ? path1 : path2}`}
-          className={`Icon-check ${props.completed && 'Icon-check--active icon'}`}
-          onClick={props.onComplete}
+          src={`${completed ? path1 : path2}`}
+          className={`Icon-check ${completed && 'Icon-check--active icon'}`}
+          onClick={onComplete}
         />
         <div className='container_Todo_Name'>
-          <p className='TodoItem-p1'>Task {numberOfTask(props.id)}</p>
-          <p id='text_item' className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
-            {props.text}
+          <p className='TodoItem-p1'>Task {numberOfTask(id)}</p>
+          <p id='text_item' className={`TodoItem-p ${completed && 'TodoItem-p--complete'}`}>
+            {text}
           </p>
         </div>
         <img
--- src/9AdviceDelete/index.js
+++ src/9AdviceDelete/index.js
@@ -1,27 +1,25 @@
 import React from "react";
-import { TodoContext } from "../TodoContext";
 import './AdviceDelete.css';
 
 // Imagenes svg
 import { ReactComponent as Trash }  from "../assets/trash.svg";
-function AdviceDelete(props){
-
-    const {deleteTodo} = React.useContext(TodoContext);
+function AdviceDelete({todoText, setOpenModal, id, deleteTodo}){
+    
 
     const onClickClose = () => {
-        props.setOpenModal(false);
+        setOpenModal(false);
     }
     const  onClickDelete = () => {
-        deleteTodo(props.id);
-        props.setOpenModal(false);
+        deleteTodo(id);
+        setOpenModal(false);
     }
     return(
         <div className="Form">
             <div className="circle_icon_trash">
                 <Trash className="icon_trash"/>
             </div>
-            <h3 className="tittle">Are you sure to delete your task?</h3>
-            <h4 className="task">'{props.todoText}'</h4>
+            <h3 className="tittle">Are you sure to delete your task (id = {id})?</h3>
+            <h4 className="task">'{todoText}'</h4>
             <p>Your task will be delete from de list of Todos!!</p>
             <div className="container_buttons">
                 <button onClick={onClickClose}>Cancel</button>
