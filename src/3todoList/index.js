b73a9fa7a7bae1b959b84ceb1e696dbfbedce37c
Angel <angelgabriel.hernandez@gmail.com>
2022-06-23 10:18:46 -0500

    Centering Todo items

--- src/3todoList/index.js
+++ src/3todoList/index.js
@@ -6,27 +6,27 @@ function TodoList(props){
     return(
         <section className="TodoList-container">
 
-        {!props.loading && (props.totalTodos === 0) && props.onNonTodos()}
+            {!props.loading && (props.totalTodos === 0) && props.onNonTodos()}
 
-        {!props.loading && (props.totalTodos === 0) && props.onCreateNewTodo()}
+            {!props.loading && (props.totalTodos === 0) && props.onCreateNewTodo()}
 
-        {props.error && props.onError() }
+            {props.error && props.onError() }
 
-        {(!props.loading && !props.searchedTodos.length && (props.totalTodos !== 0)) && props.onEmptyTodos()}
+            {(!props.loading && !props.searchedTodos.length && (props.totalTodos !== 0)) && props.onEmptyTodos()}
 
-        <div className="container_problems">
-            {props.loading &&
-                Array(3)
-                .fill(1)
-                .map((a, i) => props.onLoading())
-            }
-        </div>
+            <div className="container_problems">
+                {props.loading &&
+                    Array(3)
+                    .fill(1)
+                    .map((a, i) => props.onLoading())
+                }
+            </div>
 
-        {props.searchedTodos.map(props.render)}
+            {props.searchedTodos.map(props.render)}
 
-            <ul>
-                {props.children}
-            </ul>
+                <ul>
+                    {props.children}
+                </ul>
         </section>
     );
 }
